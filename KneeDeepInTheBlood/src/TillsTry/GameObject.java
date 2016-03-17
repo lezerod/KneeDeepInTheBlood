67be7e0b1e67c343d7d589f2c0428b5283c11fda
lezerod <lezerod@web.de>
2016-03-17 14:56:53 +0100

    Take it. Hold it. Love it.

---
+++ KneeDeepInTheBlood/src/TillsTry/GameObject.java
@@ -0,0 +1,58 @@
+package TillsTry;
+
+public class GameObject 
+{
+	public static final float VISIBLE_DURATION = 1.3f;
+	
+	public int x, y, width, height;
+	
+	public float visibleTime;
+	
+	public boolean isClicked;
+	public boolean isRemovable;
+	
+	public GameObject(int x, int y, int width, int height) 
+	{
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.visibleTime = of;
+		this.isClicked = false;
+		this.isRemovable = false;
+	}
+	
+	public void update(float delta)
+	{
+		visibleTime+= delta;
+		
+		if(visibleTime >= VISIBLE_DURATION || isClicked)
+		{
+			isRemovable = true;
+		}
+	}
+	
+	public boolean overlaps(int x, int y, int width, int height)
+	{
+		if(this.x + this.width >= x && this.x <= x + width)
+		{
+			if(this.y + this.height >= y && this.y <= y + height)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean contains(int x, int y)
+	{
+		if(x >= this.x && x <= this.x + this.width)
+		{
+			if(y >= this.y && y <= this.y + this.height)
+				return true;
+		}
+		return false;
+	}
+	
+	
+}
---
+++ KneeDeepInTheBlood/src/TillsTry/gameWorld.java
@@ -0,0 +1,83 @@
+package TillsTry;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class gameWorld 
+{
+	public static final int WORLD_X = 640;
+	public static final int WORLD_Y = 480;
+	
+	public static final int MAX_OBJECT_WIDTH = 160;
+	public static final int MIN_OBJECT_WIDTH = 80;
+	
+	public ArrayList<GameObject>objects;
+	
+	private float spawnTime;
+	private float spawnPause;
+	private Random positionRandomizer;
+	
+	public int lifes;
+	public int score;
+	
+	public gameWorld() 
+	{
+		objects = new ArrayList<GameObject>();
+		spawnTime = of;
+		spawnPause = 1.0f;
+		positionRandomizer = new Random();
+		lifes = 3;
+		score = 0;
+	}
+	
+	public void update(float delta)
+	{
+		spawnTime += delta;
+		
+		if(spawnTime >= spawnPause)
+		{
+			spawnObject();
+		}
+		
+		for(int i = 0; i < objects.size(); i++)
+		{
+			GameObject tmp = objects.get(i);
+			
+			tmp.update(delta);
+			
+			if(tmp.isRemovable)
+			{
+				if (!tmp.isClicked)
+					lifes--;
+				else
+					score++;
+				
+				objects.remove(i);
+			}
+		}
+		
+		private void spawnObject()
+		{
+			int width = positionRandomizer.nextInt((MAX_OBJECT_WIDTH - MIN_OBJECT_WIDTH) + 1) + MIN_OBJECT_WIDTH;
+			int height = positionRandomizer.nextInt((MAX_OBJECT_WIDTH - MIN_OBJECT_WIDTH + 1) + MIN_OBJECT_WIDTH);
+			
+			int x = positionRandomizer.nextInt(WORLD_X - width);
+			int y = positionRandomizer.nextInt(WORLD_Y - height);
+			
+			GameObject objTmp = new GameObject(x, y, width, height);
+			
+			for(GameObject obj : objects)
+			{
+				if(objTmp.overlaps(obj)
+						{
+							spawnObject();
+							return;
+						}
+			}
+		}
+	}
+	
+		
+	
+	
+}
