94a3ca1bb2258faa90537b1079c032935017feea
Julien <Julien@172.31.13.71>
2016-04-06 12:59:45 +0200

    tillstry gelöscht

--- KneeDeepInTheBlood/src/TillsTry/Game.java
+++
@@ -1,8 +0,0 @@
-package TillsTry;
-/*
- * nicht relevant
- */
-public class Game extends Thread
-{
-	Thread gameThread = new Thread();
-}
--- KneeDeepInTheBlood/src/TillsTry/image.java
+++
@@ -1,6 +0,0 @@
-package TillsTry;
-
-public class image 
-{
-
-}
