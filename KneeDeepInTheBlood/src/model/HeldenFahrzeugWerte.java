60054b38eff2b3ae9504b35b101fad3929c62ab7
lezerod <lezerod@web.de>
2016-04-11 17:45:13 +0200

    besser

--- KneeDeepInTheBlood/src/model/HeldenFahrzeugWerte.java
+++ KneeDeepInTheBlood/src/model/HeldenFahrzeugWerte.java
@@ -13,6 +13,6 @@ public interface HeldenFahrzeugWerte
 	final static int winkelbeispawn = 0;
 	final static int speedbeispawn = 0;
 	final static int healthbeispawn = 100;
-	Waffe waffe = new Waffe("AlienBlaster", new Effekt("Damage", 5){});
-	boolean immun = false;
+	final static Waffe waffe = new Waffe("AlienBlaster", new Effekt("Damage", 5){});
+	final static boolean immun = false;
 }
