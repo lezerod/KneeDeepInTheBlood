4779cd696c76b706416fed19e58abb0d81db2d41
lezerod <lezerod@users.noreply.github.com>
2016-05-02 10:03:30 +0200

    Update GameObject.java

--- KneeDeepInTheBlood/src/model/GameObject.java
+++ KneeDeepInTheBlood/src/model/GameObject.java
@@ -1,45 +1,42 @@
 package model;
+
 /**
  * Diese Klasse erbt von {@link SpielfeldObject}.
+ * 
  * @author til
  *
  */
-public abstract class GameObject 
-{
-
-	private int winkel = 0;
-	private int speed = 0;
-	private int health = 0;
-	private Position positon = new Position(0, 0);
+public class GameObject {
+	private float x = 0;
+	private float y = 0;
 	private int width = 0;
 	private int height = 0;
-	private boolean setting;
 	
-	public void move(){}
+	public float getX() {
+		return x;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public float getY() {
+		return y;
+	}
+	public void setY(float y) {
+		this.y = y;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
 	
-	int getWinkel() {
-		return winkel;
-	}
-
-	public void setWinkel(int winkel) {
-		this.winkel = winkel;
-	}
-
-	public int getSpeed() {
-		return speed;
-	}
-
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
-
-	public int getHealth() {
-		return health;
-	}
-
-	public void setHealth(int health) {
-		this.health = health;
-	}
-
+	
 
 }
