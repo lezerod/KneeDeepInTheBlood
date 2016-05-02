ee94c88451be0db6665b337049f44ae7cfa487e5
lezerod <lezerod@users.noreply.github.com>
2016-05-02 10:03:44 +0200

    Update GameWorld.java

--- KneeDeepInTheBlood/src/model/GameWorld.java
+++ KneeDeepInTheBlood/src/model/GameWorld.java
@@ -2,78 +2,56 @@ package model;
 
 import java.util.ArrayList;
 import java.util.Objects;
+
 /**
- * Mit dieser Klasse laesst sich das Grundgeruest der Gameworld erzeugen. Sie besitzt die Integer World_X und Y um die Groeﬂe des Spielfeldes einzustellen.
- * Auﬂerdem besitzt einen integer namens life um die Leben des Spielers festzustellen und den Integer score um die Punkte zu zaehlebn die der Spieler sammelt 
- * waehrend des Spielens. In der ArrayListe {@link Objects} werden die erstellten Objekte gespeichert die auf dem Spielfeld liegen. Im Konstruktor werden
- * den Variablen Werte zugewiesen.
+ * Mit dieser Klasse laesst sich das Grundgeruest der Gameworld erzeugen. Sie
+ * besitzt die Integer World_X und Y um die Groe√üe des Spielfeldes einzustellen.
+ * Au√üerdem besitzt einen integer namens life um die Leben des Spielers
+ * festzustellen und den Integer score um die Punkte zu zaehlebn die der Spieler
+ * sammelt waehrend des Spielens. In der ArrayListe {@link Objects} werden die
+ * erstellten Objekte gespeichert die auf dem Spielfeld liegen. Im Konstruktor
+ * werden den Variablen Werte zugewiesen.
+ * 
  * @author til
  *
  */
-public class GameWorld 
-{
+public class GameWorld {
 	private final int Wolrd_X;
 	private final int World_Y;
-	
-	private ArrayList<SpielfeldObject>objects;
-	
-	private int lifes = 0;
+	private MoveabelObject heldenfahrzeug;
+	private ArrayList<GameObject> objects;
+
 	private int score = 0;
-	
-	
-	
-	public ArrayList<SpielfeldObject> getObjects() {
+
+	public ArrayList<GameObject> getObjects() {
 		return objects;
 	}
 
-
-
-	public void setObjects(ArrayList<SpielfeldObject> objects) {
+	public void setObjects(ArrayList<GameObject> objects) {
 		this.objects = objects;
 	}
-
-
-
-	public int getLifes() {
-		return lifes;
-	}
-
-
-
-	public void setLifes(int lifes) {
-		this.lifes = lifes;
-	}
-
-
 
 	public int getScore() {
 		return score;
 	}
 
-
-
 	public void setScore(int score) {
 		this.score = score;
 	}
-
-
 
 	public int getWolrd_X() {
 		return Wolrd_X;
 	}
 
-
-
 	public int getWorld_Y() {
 		return World_Y;
 	}
-	public GameWorld()
-	{
+
+	public GameWorld() {
 		Wolrd_X = 640;
 		World_Y = 480;
-		objects = new ArrayList<SpielfeldObject>();
-		lifes = 3;
+		objects = new ArrayList<GameObject>();
 		score = 0;
 	}
-	
+
 }
